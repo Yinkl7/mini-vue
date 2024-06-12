864b3a7b37a9959b7ab00d317c9fc702ecfb038f
Yinkl7 <381964421@qq.com>
2024-06-12 10:30:09 +0800

    增加元素属性更新

--- example/update/App.js
+++ example/update/App.js
@@ -6,10 +6,15 @@ export const App = {
     window.self = this
     return h(
       'div', 
-      {},
+      {
+        ...this.elementProps
+      },
       [
         h('div', {}, 'count, ' + this.count),
-        h('button', { onClick: this.onClick }, '+1')
+        h('button', { onClick: this.onClick }, '+1'),
+        h('button', { onClick: this.changeElelmentProps }, 'changeElelmentProps'),
+        h('button', { onClick: this.changeNullProps }, 'changeNullProps'),
+        h('button', { onClick: this.deleteBarProps }, 'deleteBarProps')
       ]
     )
   },
@@ -17,9 +22,32 @@ export const App = {
   setup() {
     const count = ref(0)
     const onClick = () => count.value++
+
+    const elementProps = ref({
+      foo: 'foo',
+      bar: 'bar'
+    })
+
+    const changeElelmentProps = () => {
+      elementProps.value.foo = 'new-foo'
+    }
+
+    const changeNullProps = () => {
+      elementProps.value.foo = undefined
+    }
+
+    const deleteBarProps = () => {
+      elementProps.value = {
+        foo: 'foo'
+      }
+    }
     return {
       count,
-      onClick
+      onClick,
+      elementProps,
+      changeElelmentProps,
+      changeNullProps,
+      deleteBarProps
     }
   }
 }
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,5 +1,5 @@
 import { effect } from "../reactivity"
-import { isObject } from "../shared"
+import { EMPTY_OBJ, isObject } from "../shared"
 import { ShapeFlags } from "../shared/shapeFlags"
 import { createComponentInstance, setupComponent } from "./component"
 import { createAppApi } from "./createApp"
@@ -16,10 +16,10 @@ export function createRenderer(options) {
 
   function render(vnode, container) {
     // debugger
-    patch(null ,vnode, container, null)
+    patch(null, vnode, container, null)
   }
 
-  
+
   function patch(n1, n2, container: any, parentComponent: null) {
     // todo 判断类型 执行对应的process函数
     // 根据vnode.type的类型判断 element 和 component
@@ -49,7 +49,7 @@ export function createRenderer(options) {
   }
 
   function processElement(n1, n2, container, parentComponent) {
-    if(!n1) {
+    if (!n1) {
       mountElement(n2, container, parentComponent)
     } else {
       patchElement(n1, n2, container)
@@ -59,6 +59,35 @@ export function createRenderer(options) {
   function patchElement(n1, n2, container) {
     console.log('patchElement')
     // todo 更新内容
+    const oldProps = n1.props || EMPTY_OBJ
+    const newProps = n2.props || EMPTY_OBJ
+
+    // const el = n1.el
+    const el = (n2.el = n1.el)
+
+    patchProps(el, oldProps, newProps)
+  }
+
+  function patchProps(el, oldProps, newProps) {
+    if (oldProps !== newProps) {
+      for (const key in newProps) {
+        const prevProp = oldProps[key]
+        const newProp = newProps[key]
+
+        if (prevProp !== newProp) {
+          hostPatchProps(el, key, prevProp, newProp)
+        }
+      }
+
+      if(oldProps !== EMPTY_OBJ) {
+        for (const key in oldProps) {
+          if (!(key in newProps)) {
+            hostPatchProps(el, key, oldProps[key], null)
+          }
+        }
+      }
+    }
+
   }
 
   function mountComponent(initialVNode: any, container, parentComponent) {
@@ -82,8 +111,7 @@ export function createRenderer(options) {
 
     for (const key in props) {
       const val = props[key]
-
-      hostPatchProps(el, key, val)
+      hostPatchProps(el, key, null, val)
     }
 
     hostInsert(el, container)
