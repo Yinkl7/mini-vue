730e30ff52ae2fdd4c17c68856f51e9dcdf77dc1
Yinkl7 <381964421@qq.com>
2024-06-13 15:02:00 +0800

    完善diff算法

--- example/patchChildren/ArrayToArray.js
+++ example/patchChildren/ArrayToArray.js
@@ -30,21 +30,21 @@ import { h, ref } from "../../lib/guide-mini-vue.esm.js"
 //   h('p', {key: 'C'}, 'C'),
 // ]
 
-// 新数组元素大于老数组的元素 创建元素
+// 1、新数组元素大于老数组的元素 创建元素
 // ab => abc
-const prevChildren = [
-  h('p', {key: 'A'}, 'A'),
-  h('p', {key: 'B'}, 'B')
-]
+// const prevChildren = [
+//   h('p', {key: 'A'}, 'A'),
+//   h('p', {key: 'B'}, 'B')
+// ]
 
-const nextChildren = [
-  h('p', {key: 'A'}, 'A'),
-  h('p', {key: 'B'}, 'B'),
-  h('p', {key: 'C'}, 'C'),
-  h('p', {key: 'D'}, 'D'),
-]
+// const nextChildren = [
+//   h('p', {key: 'A'}, 'A'),
+//   h('p', {key: 'B'}, 'B'),
+//   h('p', {key: 'C'}, 'C'),
+//   h('p', {key: 'D'}, 'D'),
+// ]
 
-ab => cab
+// ab => cab
 // const prevChildren = [
 //   h('p', {key: 'A'}, 'A'),
 //   h('p', {key: 'B'}, 'B')
@@ -57,7 +57,7 @@ ab => cab
 //   h('p', {key: 'B'}, 'B'),
 // ]
 
-// 新数组元素小于老数组的元素 删除元素
+// 2、新数组元素小于老数组的元素 删除元素
 // 删除右侧
 // const prevChildren = [
 //   h('p', {key: 'A'}, 'A'),
@@ -81,6 +81,113 @@ ab => cab
 //   h('p', {key: 'B'}, 'B'),
 //   h('p', {key: 'C'}, 'C'),
 // ]
+
+// 3、中间对比
+// ab (cd) fg => ab (ec) fg
+
+// const prevChildren = [
+//   h('p', {key: 'A'}, 'A'),
+//   h('p', {key: 'B'}, 'B'),
+//   h('p', {key: 'C', id: 'c-prev'}, 'C'),
+//   h('p', {key: 'D'}, 'D'),
+//   h('p', {key: 'F'}, 'F'),
+//   h('p', {key: 'G'}, 'G'),
+// ]
+
+// const nextChildren = [
+//   h('p', {key: 'A'}, 'A'),
+//   h('p', {key: 'B'}, 'B'),
+//   h('p', {key: 'E'}, 'E'),
+//   h('p', {key: 'C', id: 'c-next'}, 'C'),
+//   h('p', {key: 'F'}, 'F'),
+//   h('p', {key: 'G'}, 'G'),
+// ]
+
+// const prevChildren = [
+//   h('p', {key: 'A'}, 'A'),
+//   h('p', {key: 'B'}, 'B'),
+//   h('p', {key: 'C', id: 'c-prev'}, 'C'),
+//   h('p', {key: 'E'}, 'E'),
+//   h('p', {key: 'D'}, 'D'),
+//   h('p', {key: 'F'}, 'F'),
+//   h('p', {key: 'G'}, 'G'),
+// ]
+
+// const nextChildren = [
+//   h('p', {key: 'A'}, 'A'),
+//   h('p', {key: 'B'}, 'B'),
+//   h('p', {key: 'E'}, 'E'),
+//   h('p', {key: 'C', id: 'c-next'}, 'C'),
+//   h('p', {key: 'F'}, 'F'),
+//   h('p', {key: 'G'}, 'G'),
+// ]
+
+// 移动元素
+// const prevChildren = [
+//   h('p', {key: 'A'}, 'A'),
+//   h('p', {key: 'B'}, 'B'),
+//   h('p', {key: 'C', id: 'c-prev'}, 'C'),
+//   h('p', {key: 'D'}, 'D'),
+//   h('p', {key: 'E'}, 'E'),
+//   h('p', {key: 'F'}, 'F'),
+//   h('p', {key: 'G'}, 'G'),
+// ]
+
+// const nextChildren = [
+//   h('p', {key: 'A'}, 'A'),
+//   h('p', {key: 'B'}, 'B'),
+//   h('p', {key: 'E'}, 'E'),
+//   h('p', {key: 'C', id: 'c-prev'}, 'C'),
+//   h('p', {key: 'D'}, 'D'),
+//   h('p', {key: 'F'}, 'F'),
+//   h('p', {key: 'G'}, 'G'),
+// ]
+
+// 移动并创建新、删除节点
+// const prevChildren = [
+//   h('p', {key: 'A'}, 'A'),
+//   h('p', {key: 'B'}, 'B'),
+//   h('p', {key: 'C', id: 'c-prev'}, 'C'),
+//   h('p', {key: 'D'}, 'D'),
+//   h('p', {key: 'Y'}, 'Y'),
+//   h('p', {key: 'E'}, 'E'),
+//   h('p', {key: 'F'}, 'F'),
+//   h('p', {key: 'G'}, 'G'),
+// ]
+
+// const nextChildren = [
+//   h('p', {key: 'A'}, 'A'),
+//   h('p', {key: 'B'}, 'B'),
+//   h('p', {key: 'E'}, 'E'),
+//   h('p', {key: 'C', id: 'c-next'}, 'C'),
+//   h('p', {key: 'D'}, 'D'),
+//   h('p', {key: 'H'}, 'H'),
+//   h('p', {key: 'F'}, 'F'),
+//   h('p', {key: 'G'}, 'G'),
+// ]
+
+const prevChildren = [
+  h('p', {key: 'A'}, 'A'),
+  h('p', {key: 'B'}, 'B'),
+  h('p', {key: 'C', id: 'c-prev'}, 'C'),
+  h('p', {key: 'D'}, 'D'),
+  h('p', {key: 'E'}, 'E'),
+  h('p', {key: 'Z'}, 'Z'),
+  h('p', {key: 'F'}, 'F'),
+  h('p', {key: 'G'}, 'G'),
+]
+
+const nextChildren = [
+  h('p', {key: 'A'}, 'A'),
+  h('p', {key: 'B'}, 'B'),
+  h('p', {key: 'D'}, 'D'),
+  h('p', {key: 'C', id: 'c-next'}, 'C'),
+  h('p', {key: 'y'}, 'Y'),
+  h('p', {key: 'E'}, 'E'),
+  h('p', {key: 'F'}, 'F'),
+  h('p', {key: 'G'}, 'G'),
+]
+
 
 export const ArrayToArray = {
   render() {
